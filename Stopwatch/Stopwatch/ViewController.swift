59fbaa242ac65eec5305c865569fc53a735f788c
Stanislav Dimitrov <stanislavdimitrov@Stanislavs-iMac.local>
2016-11-18 16:49:37 +0200

    feat(intro.swift): new file

--- Stopwatch/Stopwatch/ViewController.swift
+++ Stopwatch/Stopwatch/ViewController.swift
@@ -48,8 +48,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         lapResetButton.setTitle("Reset", forState: .Normal)
             print()
 
-            print(34)
-            print(45)
+
         }
     }
     
